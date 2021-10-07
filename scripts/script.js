d2d7d8a66d812b93c9fe9f816c43abb6ca88b92f
Stefan Leusink <0318435@student.rocvantwente.nl>
2021-10-07 22:21:20 +0200

    master

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const navigationButton = document.querySelector('.navigation__button');
+const navigation = document.querySelector('.navigation')
+
+navigationButton.addEventListener('click', () => {
+    navigation.classList.toggle('navigation--open');
+});
