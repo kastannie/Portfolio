9887bd10346eca9394d21677f512f650bc3fc3dc
Stefan Leusink <0318435@student.rocvantwente.nl>
2021-12-23 21:20:51 +0100

    master

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const navigationButton = document.querySelector('.navigation__button');
-const navigation = document.querySelector('.navigation')
+const navigationButton = document.querySelector(".navigation__button");
+const navigation = document.querySelector(".navigation");
 
-navigationButton.addEventListener('click', () => {
-    navigation.classList.toggle('navigation--open');
+navigationButton.addEventListener("click", () => {
+  navigation.classList.toggle("navigation--open");
 });
